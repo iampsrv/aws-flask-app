30c01df73dddcbbb0c41504aca22133bb9f77ec3
Pranjal Srivastava <75536136+iampsrv@users.noreply.github.com>
2021-10-09 21:35:46 +0530

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-virtualenv venv
-cd /home/ubuntu/new/aws-flask-app
-cd venv/bin
-. activate
-cd /home/ubuntu/new/aws-flask-app
-pip install -r requirements.txt
-python application.py
+python3 application.py
